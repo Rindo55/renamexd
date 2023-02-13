d5ec7a36bc8bdd0e4de5ecf75aac46352b82a713
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-13 21:31:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,30 @@ from config import *
 from route import web_server
 from aiohttp import web
 
-    async def start(self):  
+class Bot(Client):
+    def init(self):
+        super().init(
+            name="simple-renamer",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "main"},
+            sleep_threshold=10,
+        )
+    async def start(self):
+        await super().start()
+        me = await self.get_me()   
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         port = "8080"
         await web.TCPSite(app, bind_address, port).start()   
-        print(f"{"hello"} | bot 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
        
+    async def stop(self, *args):
+       await super().stop()      
+       print("Bot Restarting........")
 
 
 bot = Bot()
