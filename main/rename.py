dc977adf56757e48675e0020aeafb46057c4acfe
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-13 21:16:09 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -4,37 +4,44 @@ from pyrogram import Client, filters, enums
 from config import temp, CAPTION, ADMIN
 from main.utils import progress_message, humanbytes
 
-@Client.on_message(filters.private & filters.document & filters.user(ADMIN))             
+@Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))             
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
-    cos = msg.id
-    new_namex = "hello"
+    if len(msg.command) < 2 or not reply:
+       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    media = reply.document or reply.audio or reply.video
+    if not media:
+       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    og_media = getattr(reply, reply.media.value)
+    new_name = msg.text.split(" ", 1)[1]
+    new_namex = new_name.replace(".mkv", "")
     sts = await msg.reply_text("Trying to Downloading.....")
     c_time = time.time()
-    downloaded = await msg.download(progress=progress_message, progress_args=("Download Started.....", sts, c_time))                 
-   
+    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
+    filesize = humanbytes(og_media.file_size)                
     if CAPTION:
         try:
-            cap = CAPTION.format(file_name=new_namex)
+            cap = CAPTION.format(file_name=new_name, file_size=filesize)
         except Exception as e:            
             await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
             return
     else:
         cap = f"{new_namex}"
+    raw_thumbnail = temp.THUMBNAIL 
+    if raw_thumbnail:
+        og_thumbnail = await bot.download_media(raw_thumbnail)
+    else:
+        og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
     await sts.edit("Trying to Uploading")
     c_time = time.time()
-    filename = os.path.basename(downloaded)
-    filename = filename.replace("720p", "720p x265 BD")
-    filename = filename.replace("@Anime_Gallery", "@animxt")
-    filename = filename.replace("[Judas]", "")
-    filename = filename.replace(".mkv", " [1080p x265 BD][Dual] @animxt.mkv")
     try:
-        await bot.send_document(msg.chat.id, document=downloaded, file_name=filename, caption=filename.replace(".mkv", ""), progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+        await bot.send_document(msg.chat.id, document=downloaded, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
     except Exception as e:  
         await sts.edit(f"Error {e}") 
         return               
     try:
         os.remove(downloaded)
+        os.remove(og_thumbnail)
     except:
         pass
     await sts.delete()
